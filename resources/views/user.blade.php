d97e430fd3c51ac895c45ece94ce860802b05f57
yinkyaewai <yinkyae3299@gmail.com>
2025-02-05 16:13:14 +0630

    add animation to user cell

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,100 +1,132 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>Laravel</title>
 
-        <style>
-            .user-cell{
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <style>
+        .user-cell {
+            width: 100px;
+            height: 30px;
+            border: 1px dotted gray;
+            text-align: center;
+            cursor: pointer;
+            margin: 4px;
+        }
+
+        table {
+            margin: auto;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .beautify-cell {
+            animation: cell-animation 5s linear 0.1ms 3;
+        }
+
+        @keyframes cell-animation {
+            from {
+
                 width: 100px;
                 height: 30px;
                 border: 1px dotted gray;
                 text-align: center;
                 cursor: pointer;
+                margin: 4px;
 
             }
-            table{
-                margin: auto;
-            }
-            h1{
+
+            to {
+                width: 160px;
+                height: 50px;
+                border: 1px solid green;
                 text-align: center;
+                cursor: pointer;
+                margin: 50px;
             }
-        </style>
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <h1>User Table</h1>
-      <table>
+        }
+    </style>
+</head>
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <h1>User Table</h1>
+    <table>
         <tr>
-            <td class="user-cell">No</td>
-            <td class="user-cell">Name</td>
-            <td class="user-cell">phone</td>
-            <td class="user-cell">Address</td>
-            <td class="user-cell">Link</td>
+            <td class="user-cell beautify-cell">No</td>
+            <td class="user-cell beautify-cell">Name</td>
+            <td class="user-cell beautify-cell">phone</td>
+            <td class="user-cell beautify-cell">Address</td>
+            <td class="user-cell beautify-cell">Link</td>
         </tr>
         <tr>
-            <td class="user-cell">1</td>
-            <td class="user-cell">Ma Ma</td>
-            <td class="user-cell">01111111</td>
-            <td class="user-cell">Mandalay</td>
-            <td class="user-cell"><a href="/">Welcome</a></td>
+            <td class="user-cell beautify-cell">1</td>
+            <td class="user-cell beautify-cell">Ma Ma</td>
+            <td class="user-cell beautify-cell">01111111</td>
+            <td class="user-cell beautify-cell">Mandalay</td>
+            <td class="user-cell beautify-cell"><a href="/">Welcome</a></td>
         </tr>
         <tr>
-            <td class="user-cell">2</td>
-            <td class="user-cell">Su Su</td>
-            <td class="user-cell">02222222</td>
-            <td class="user-cell">Yangon</td>
-            <td class="user-cell"><a href="/about-us">About Us</a></td>
+            <td class="user-cell beautify-cell">2</td>
+            <td class="user-cell beautify-cell">Su Su</td>
+            <td class="user-cell beautify-cell">02222222</td>
+            <td class="user-cell beautify-cell">Yangon</td>
+            <td class="user-cell beautify-cell"><a href="/about-us">About Us</a></td>
         </tr>
         <tr>
-            <td class="user-cell">3</td>
-            <td class="user-cell">Aye Aye</td>
-            <td class="user-cell">03333333</td>
-            <td class="user-cell">Taungyi</td>
-            <td class="user-cell"><a href="/contact-us">Contact Us</a></td>
+            <td class="user-cell beautify-cell">3</td>
+            <td class="user-cell beautify-cell">Aye Aye</td>
+            <td class="user-cell beautify-cell">03333333</td>
+            <td class="user-cell beautify-cell">Taungyi</td>
+            <td class="user-cell beautify-cell"><a href="/contact-us">Contact Us</a></td>
         </tr>
         <tr>
-            <td class="user-cell">4</td>
-            <td class="user-cell">Ma Ma</td>
-            <td class="user-cell">04444444</td>
-            <td class="user-cell">Lashio</td>
-            <td class="user-cell"><a href="/about-us">About Us</a></td>
+            <td class="user-cell beautify-cell">4</td>
+            <td class="user-cell beautify-cell">Ma Ma</td>
+            <td class="user-cell beautify-cell">04444444</td>
+            <td class="user-cell beautify-cell">Lashio</td>
+            <td class="user-cell beautify-cell"><a href="/about-us">About Us</a></td>
         </tr>
         <tr>
-            <td class="user-cell">5</td>
-            <td class="user-cell">Ei Ei</td>
-            <td class="user-cell">05555555</td>
-            <td class="user-cell">Monywa</td>
-            <td class="user-cell"><a href="/contact-us">Contact Us</a></td>
+            <td class="user-cell beautify-cell">5</td>
+            <td class="user-cell beautify-cell">Ei Ei</td>
+            <td class="user-cell beautify-cell">05555555</td>
+            <td class="user-cell beautify-cell">Monywa</td>
+            <td class="user-cell beautify-cell"><a href="/contact-us">Contact Us</a></td>
         </tr>
         <tr>
-            <td class="user-cell">6</td>
-            <td class="user-cell">Aung Aung</td>
-            <td class="user-cell">06666666</td>
-            <td class="user-cell">Monywa</td>
-            <td class="user-cell"><a href="/contact-us">Contact Us</a></td>
+            <td class="user-cell beautify-cell">6</td>
+            <td class="user-cell beautify-cell">Aung Aung</td>
+            <td class="user-cell beautify-cell">06666666</td>
+            <td class="user-cell beautify-cell">Monywa</td>
+            <td class="user-cell beautify-cell"><a href="/contact-us">Contact Us</a></td>
         </tr>
         <tr>
-            <td class="user-cell">7</td>
-            <td class="user-cell">Myo Myo</td>
-            <td class="user-cell">07777777</td>
-            <td class="user-cell">Monywa</td>
-            <td class="user-cell"><a href="/contact-us">Contact Us</a></td>
+            <td class="user-cell beautify-cell">7</td>
+            <td class="user-cell beautify-cell">Myo Myo</td>
+            <td class="user-cell beautify-cell">07777777</td>
+            <td class="user-cell beautify-cell">Monywa</td>
+            <td class="user-cell beautify-cell"><a href="/contact-us">Contact Us</a></td>
         </tr>
-      </table>
-    </body>
+    </table>
+</body>
+
 </html>
 
 <script>
-    $(document).ready(function () {
-        $('.user-cell').click(function () {
-            $(this).text('hello');
-        })
+    $(document).ready(function() {
+        // $('.user-cell').click(function() {
+        //     $(this).text('hello');
+        //     $(this).addClass('beautify-cell')
+        // })
     })
 </script>
